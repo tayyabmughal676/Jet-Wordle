b330ef59202938d9ac8f27a9678cddf58556862e
Tayyab Mughal <tayyabmughal676@gmail.com>
2022-03-20 09:46:44 +0500

    Initial Commit

---
+++ Jet-Wordle/Jet_WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jet_WordleApp.swift
+//  Jet-Wordle
+//
+//  Created by Thor on 20/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Jet_WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
